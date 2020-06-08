a89b6cc90121cb9de15cc50b4ed7233038a20948
AndradeSan <40698893+AndradeSan@users.noreply.github.com>
2020-06-08 05:10:00 -0500

    bueno

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const express = require ('express');
 const app = express();
 
-<<<<<<< HEAD
 app.get('/', (req,res) =>res.send('Hola Mundo Aton Nuevo'));
-//dfgdfgdfg
-
 
 app.listen(3000);
 console.log('Server on Port',3000);
 
 
-
-///dsfgdfg
-
-///sadfsdf
-
-
-
-=======
-app.get('/', (req,res) =>res.send('Hola Mundo new'));
-
-app.listen(3000);
-console.log('Server on Port',3000);
->>>>>>> a78afe1d1d87576ed8aa8b655debb329a94e4445
