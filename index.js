0acc92fc7a3a599cbbaa1634be9705322b13a505
AndradeSan <40698893+AndradeSan@users.noreply.github.com>
2020-06-08 04:56:30 -0500

    Aton

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 <<<<<<< HEAD
-app.get('/', (req,res) =>res.send('Hola Mundo new 123 \n'));
+app.get('/', (req,res) =>res.send('Hola Mundo Aton Nuevo'));
 //dfgdfgdfg
 
 
